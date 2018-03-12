c2f5dbd117a65ac068027e6a3c2972a9f0be1c86
zhugedongming <zhugedongming@sogou-inc.com>
2018-03-12 20:36:32 +0800

    打开清华、北邮等论坛的配置

--- Main.py
+++ Main.py
@@ -3,17 +3,18 @@
 from CPublishmanager import CPublish_manager_byr
 from CPublishmanager import CPublish_manager_newsmth
 from CPublishmanager import CPublish_manager_buaaer
+from CPublishmanager import CPublish_manager_bjtu
 
 if __name__ == "__main__":
-    # manager_byr = CPublish_manager_byr("byr")
-    # manager_byr.do_publish()
+    manager_byr = CPublish_manager_byr("byr")
+    manager_byr.do_publish()
 
-    # manager_newsmth = CPublish_manager_newsmth("newsmth")
-    # #manager_newsmth.do_publish()
-    # manager_newsmth.do_reply()
+    manager_newsmth = CPublish_manager_newsmth("newsmth")
+    #manager_newsmth.do_publish()
+    manager_newsmth.do_reply()
 
-    # manager_buaaer = CPublish_manager_buaaer("buaaer")
-    # manager_buaaer.do_publish()
+    manager_buaaer = CPublish_manager_buaaer("buaaer")
+    manager_buaaer.do_publish()
 
-    manager_buaaer = CPublish_manager_buaaer("bjtu")
+    manager_buaaer = CPublish_manager_bjtu("bjtu")
     manager_buaaer.do_publish()
