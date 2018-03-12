495d3f3b0d2a5bec0cd34250a33427cf7b88b2a9
zhugedongming <zhugedongming@sogou-inc.com>
2018-03-12 17:38:19 +0800

    修改merge引入的Bug

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+
+from CPublishmanager import CPublish_manager_byr
+from CPublishmanager import CPublish_manager_newsmth
 
 if __name__ == "__main__":
-    # manager_byr = CPublish_manager_byr()
-    # manager_byr.do_publish()
+    manager_byr = CPublish_manager_byr()
+    manager_byr.do_publish()
 
     manager_newsmth = CPublish_manager_newsmth()
     #manager_newsmth.do_publish()
