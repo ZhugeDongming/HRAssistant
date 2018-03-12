816a9020e325154a6b943529b979220411c506db
zhugedongming <zhugedongming@sogou-inc.com>
2018-03-12 17:33:15 +0800

    Revert "修改个问题"
    This reverts commit 1fcee9cf55fba137092d8cc7ada0cce9cb511ab8.

--- Main.py
+++ Main.py
@@ -5,9 +5,10 @@ from CPublishmanager import CPublish_manager_byr
 from CPublishmanager import CPublish_manager_newsmth
 
 if __name__ == "__main__":
-    manager_byr = CPublish_manager_byr()
-    manager_byr.do_publish()
+    # manager_byr = CPublish_manager_byr()
+    # manager_byr.do_publish()
 
     manager_newsmth = CPublish_manager_newsmth()
-    manager_newsmth.do_publish()
+    #manager_newsmth.do_publish()
+    manager_newsmth.do_reply()
 
